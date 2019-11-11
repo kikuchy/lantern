ee39c945c294c17735d598a19c54adb0084b02a3
kikuchy <kikuchy.h@gmail.com>
2019-11-11 13:19:14 +0900

    add: parameter validation

--- lib/lantern.dart
+++ lib/lantern.dart
@@ -1,10 +1,15 @@
+import 'package:lantern/src/checker.dart';
 import 'package:lantern/src/generator/generator.dart';
 import 'package:lantern/src/lantern_parser.dart';
 
 List<GeneratedCodeFile> parseLantern(String source) {
   final parser = LanternParser();
+  final parameterChecker = ParameterChecker();
   final generators = [DartCodeGenerator("./"), SwiftCodeGenerator("./")];
-  final parsed = parser.parse(source).map(
+  final parsed = parser.parse(source).map((schema) {
+    parameterChecker.check(schema);
+    return schema;
+  }).map(
       (schema) => generators.map((g) => g.generate(schema)).expand((c) => c));
   return parsed.value.toList();
 }
---
+++ lib/src/checker.dart
@@ -0,0 +1,39 @@
+import 'package:lantern/src/ast.dart';
+
+class InvalidParameterError extends Error {
+  final String invalidName;
+
+  InvalidParameterError(this.invalidName);
+
+  @override
+  String toString() => "$invalidName is not acceptable parameter.";
+}
+
+class ParameterChecker {
+  static const acceptableForCollection = {"specificId"};
+  static const acceptableForDocument = {"saveCreatedDate", "saveModifiedDate"};
+
+  void _checkCollection(Collection collection) {
+    collection.params.forEach((p) {
+      if (!acceptableForCollection.contains(p.name)) {
+        throw InvalidParameterError(p.name);
+      }
+    });
+    _checkDocument(collection.document);
+  }
+
+  void _checkDocument(Document document) {
+    document.params.forEach((p) {
+      if (!acceptableForDocument.contains(p.name)) {
+        throw InvalidParameterError(p.name);
+      }
+    });
+    document.collections.forEach((c) => _checkCollection(c));
+  }
+
+  void check(Schema root) {
+    root.collections.forEach((c) => _checkCollection(c));
+  }
+}
+
+class TypeChecker {}
